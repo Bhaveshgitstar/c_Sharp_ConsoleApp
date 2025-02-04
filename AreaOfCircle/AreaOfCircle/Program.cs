224fb57739f6c13354ab05c1ae8ee8dfd8789cd7
Bhavesh Sharma <141301287+Bhaveshgitstar@users.noreply.github.com>
2025-02-04 17:04:15 +0530

    Add files via upload

---
+++ AreaOfCircle/AreaOfCircle/Program.cs
@@ -0,0 +1,10 @@
+﻿class CircleArea {
+    static void Main() { 
+        decimal pi=(22M/7M);
+        System.Console.WriteLine("Enter the Radius for circle:");
+        decimal radius= System.Convert.ToDecimal(System.Console.ReadLine());
+        decimal ansArea= pi * radius * radius;
+        System.Console.WriteLine("Area of circle is:");
+        System.Console.Write(ansArea);
+    }
+}
---
+++ Array/Array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+class Array
+{
+
+    static void PrintList(List<int> list)
+    {
+
+        foreach (var i in list)
+        {
+            Console.Write(i + " ");
+        }
+        Console.Write("\n");
+    }
+
+    static void Main()
+    {
+
+
+        List<int> list = new List<int>() { 10, 20, 30 };
+
+        Array.PrintList(list);
+        list.Add(10);
+        Array.PrintList(list);
+        list.AddRange(new List<int>() { 30, 45, 67 });
+        Array.PrintList(list);
+
+
+
+
+    }
+
+}
